d0e2f97d8d59c85dbe86db1b20bb0d1bcfe5b8d0
tkstaym1109 <67414938+tkstaym1109@users.noreply.github.com>
2020-07-12 17:47:13 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let array = ["太郎", "次郎", "三郎", "四郎", "五郎"];
+
+console.log(array);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
